95fb798c79d55cebcd884612cf4cc62f4fd64659
your_name <andreayong@Andreas-MacBook-Air.local>
2023-01-16 10:26:00 +0800

    initial commit

---
+++ test/day2workshop/AppTest.java
@@ -0,0 +1,18 @@
+package day2workshop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
